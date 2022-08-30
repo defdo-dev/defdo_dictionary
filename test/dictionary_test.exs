727f244adcb4acfcc270de3238c9b9f5d735f145
Roberto Estrada <restrada@paridin.com>
2022-08-30 11:24:31 -0500

    fix tests

--- test/dictionary_test.exs
+++ test/dictionary_test.exs
@@ -4,10 +4,10 @@ defmodule DictionaryTest do
 
   test "generate a slug" do
     # We are checking the format of the string, not the randomness
-    assert Dictionary.random_slug(["hello"], 3) == "hello-hello-hello"
+    assert Dictionary.Impl.WordList.random_slug(["hello"], 3) == "hello-hello-hello"
   end
 
   test "check we receive the path for popular dictionary" do
-    Dictionary.popular_dictionary_path() =~ "lib/dictionary/priv/popular.txt"
+    assert Dictionary.Impl.WordList.popular_dictionary_path() =~ "lib/defdo_dictionary/priv/popular.txt"
   end
 end
